b703bb3c1b4a0cd775270d0c758848bbfb3d2b41
Naveen-004 <chinna.nk4@gmail.com>
2023-02-21 13:29:53 +0530

    #2 Quality improvements

--- app.py
+++ app.py
@@ -3,17 +3,21 @@ import streamlit as st
 import snscrape.modules.twitter as sntwitter
 
 st.header('Cyberbullying Detection')
-with st.sidebar:
-    n = st.slider('Number of posts to scrape', 1, 1000, 100)
-    field = st.text_input('Enter text to search for', 'cyberbullying')
+# scrape tweets
+def scrape_tweets(field, n):
     tweet_list = []
     for i, tweet in enumerate(sntwitter.TwitterSearchScraper(field).get_items()):
         if i > n:
             break
         tweet_list.append([tweet.content])
+    return tweet_list
+
+with st.sidebar:
+    n = st.slider('Number of posts to scrape', 1, 1000, 100)
+    field = st.text_input('Enter text to search for', 'cyberbullying')
     
 
-df = pd.DataFrame(tweet_list, columns=['tweets'])
+df = pd.DataFrame(scrape_tweets(field, n), columns=['tweets'])
 st.dataframe(df)
 st.download_button(label='Download', data=df.to_csv(index=False), file_name='tweets.csv', mime='text/csv')
     
